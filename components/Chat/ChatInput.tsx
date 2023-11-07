3612b24fc8b624896df4c98831595841ab654d9f
Abhishek Shandilya <97976470+abhishandilya@users.noreply.github.com>
2023-11-07 16:33:15 -0500

    rebrand to OCPP.AI

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -324,9 +324,7 @@ export const ChatInput = ({
                   : 'hidden'
               }`,
             }}
-            placeholder={
-              t('Type a message or type "/" to select a prompt...') || ''
-            }
+            placeholder={t('Ask anything from OCPP 1.6') || ''}
             value={content}
             rows={1}
             onCompositionStart={() => setIsTyping(true)}
@@ -381,16 +379,16 @@ export const ChatInput = ({
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
-          href="https://github.com/mckaywrigley/chatbot-ui"
+          href="https://github.com/abhishandilya/ocpp.ai"
           target="_blank"
           rel="noreferrer"
           className="underline"
         >
-          ChatBot UI
+          OCPP AI
         </a>
         .{' '}
         {t(
-          "Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
+          'OCPP AI is an advanced chatbot to answer any question about Open Charge Point Protocol.',
         )}
       </div>
     </div>
